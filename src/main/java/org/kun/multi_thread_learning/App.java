a42a332895eec8e69cda4b6dbb4bafd8b9e8d069
kunkun <403879889@qq.com>
2019-08-18 11:34:44 +0800

    init the repo

---
+++ src/main/java/org/kun/multi_thread_learning/App.java
@@ -0,0 +1,13 @@
+package org.kun.multi_thread_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
